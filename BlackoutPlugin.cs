e97f0f616e0b8720df9f2f1846a6919440ca3f6f
KarolK <kkacprzak2115@gmail.com>
2023-01-27 10:47:22 +0100

    init

---
+++ BlackoutPlugin.cs
@@ -0,0 +1,35 @@
+﻿#region Usings
+using Exiled.API.Enums;
+using Exiled.API.Features;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#endregion
+
+namespace BlackoutPlugin
+{
+    public class BlackoutPlugin : Plugin<Config>
+    {
+        public static BlackoutPlugin Singleton;
+        public static BlackoutPlugin Instance => Singleton;
+        public override PluginPriority Priority { get; } = PluginPriority.Last;
+
+        public BlackoutPlugin()
+        {
+        }
+
+        public override void OnEnabled()
+        {
+            Singleton = this;
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            Singleton = null;
+            base.OnDisabled();
+        }
+    }
+}
